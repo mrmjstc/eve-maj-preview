8b4fdc798650c59cebb420442985f11f55c3e8ad
tjerge <srgdwlng@googlemail.com>
2026-08-24 10:50:53 +0100

    fix(config_dialog): capture digit keys by physical code instead of shifted char

--- src/config_dialog.js
+++ src/config_dialog.js
@@ -3625,6 +3625,16 @@ function captureKeyDown(e) {
 
 function mapMainKey(key, code) {
     const codeMap = {
+        'Digit0': '0',
+        'Digit1': '1',
+        'Digit2': '2',
+        'Digit3': '3',
+        'Digit4': '4',
+        'Digit5': '5',
+        'Digit6': '6',
+        'Digit7': '7',
+        'Digit8': '8',
+        'Digit9': '9',
         'Numpad0': 'Numpad0',
         'Numpad1': 'Numpad1',
         'Numpad2': 'Numpad2',
